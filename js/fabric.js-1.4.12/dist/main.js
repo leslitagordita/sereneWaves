cad852a018693851acbe56045d55ed7b773c9230
leslitagordita <contact@lesliesalazar.com>
2015-04-10 21:17:24 -0400

    First commit

---
+++ js/fabric.js-1.4.12/dist/main.js
@@ -0,0 +1,11 @@
+(function() {
+
+	var canvas = new fabric.Canvas('c'{
+		backgroundColor: 'rgb(100,100,200)',
+	});
+	var rect = new fabric.Rect();
+	canvas.add(rect); // add object
+
+      
+
+})();
---
+++ js/main.js
@@ -0,0 +1,12 @@
+(function() {
+
+	var canvas = new fabric.Canvas('c'{
+		backgroundColor: 'rgb(256,100,200)',
+	});
+
+	var rect = new fabric.Rect();
+	canvas.add(rect); // add object
+
+      
+
+})();
